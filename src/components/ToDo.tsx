cac1a720b5080fbcc9d475bb5899bb93720ec69b
Seongki Kim <moztiq@gmail.com>
2023-10-29 16:24:43 +0900

    categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,22 @@
-import { IToDo } from '../atoms';
+import { IToDo, toDoState } from '../atoms';
+import { useSetRecoilState } from 'recoil';
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (newCategory: IToDo['category']) => {};
   return (
     <li>
-      {text}
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      <span>{text}</span>
+      {category !== 'TO_DO' && (
+        <button onClick={() => onClick('TO_DO')}>To Do</button>
+      )}
+      {category !== 'DOING' && (
+        <button onClick={() => onClick('DOING')}>Doing</button>
+      )}
+      {category !== 'DONE' && (
+        <button onClick={() => onClick('DONE')}>Done</button>
+      )}
     </li>
   );
 }
