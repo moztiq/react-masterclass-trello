015804def2ea96f549e5fdf74b8d71510670eca2
Seongki Kim <moztiq@gmail.com>
2023-10-29 17:29:43 +0900

    selector one

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,17 +1,33 @@
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 import { useRecoilValue } from 'recoil';
-import { toDoState } from '../atoms';
+import { toDoSelector, toDoState } from '../atoms';
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const [toDos, doings, dones] = useRecoilValue(toDoSelector);
+
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
         {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doings.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {dones.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
